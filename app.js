88a5c918f383d56815ba795857e0bbbf4c6144a5
unknown <barchettaleonel@gmailcom>
2024-01-30 11:18:31 -0300

    final

---
+++ app.js
@@ -0,0 +1,11 @@
+const menu = document.getElementById("menu")
+const cerrar = document.getElementById("cerrar")
+const nav = document.getElementById("nav")
+
+menu.addEventListener("click", ()=>{
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", ()=>{
+    nav.classList.remove("visible");
+})
